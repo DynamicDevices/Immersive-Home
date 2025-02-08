0fa0224629795bb6bd26979d20854093ab11bedb
Poppy <p.curranjones@gmail.com>
2025-02-08 18:25:48 +0000

    Added tooltip editing
    Added a tooltip text editor

--- app/content/entities/tooltip/tooltip.gd
+++ app/content/entities/tooltip/tooltip.gd
@@ -2,9 +2,9 @@ extends Entity
 
 const Entity = preload ("../entity.gd")
 
-@onready var getting_started_button = $GettingStartedButton
+@onready var text_edit_button = $TextEditButton
 @onready var close_button = $CloseButton
-@onready var textbox = $Viewport2Din3D/Viewport/ScrollingTextbox
+@onready var input = $Input
 
 func _ready():
 	
@@ -12,11 +12,19 @@ func _ready():
 	close_button.on_button_down.connect(func():
 		close()
 	)
+	
+	text_edit_button.on_button_down.connect(func():
+		text_edit()
+		)
+		
+func _input(event):
+	if event is InputEventKey and Input.is_key_pressed(KEY_M):
+		print_debug("someone pressed M")
+
+func text_edit():
+	input.visible = !input.visible
 
 func close():
 	queue_free()
 
-
-func _on_getting_started_button_on_button_down() -> void:
-	textbox.add_text(JSON.parse_string(FileAccess.get_file_as_string(Store.house._save_path)))
-	pass # Replace with function body.
+	
